1d33f4e83df623fe16ebc894685791c0bf15a89e
Marta Wilczynski <32047132+mewilczynski@users.noreply.github.com>
2018-03-25 04:02:05 -0400

    Add files via upload

--- bubble fe/my-app/src/App.js
+++ bubble fe/my-app/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import login from './components/pages/login.js';
+import bubble from './bubble.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <div className="App-header">
+          <login></login>
+          <img src={bubble} />
+        </div>
+      <div className="App-body">
+        <form action="#">
+          Sign in with school email
+          <br></br>
+        <input type="text" name="username" value=""/>
+          <br></br><br></br>
+        <input type="submit" value="Submit"/>
+        </form>
+        </div>
       </div>
     );
   }
---
+++ bubble fe/my-app/src/components/pages/login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './login.css';
+
+class login extends Component {
+  render() {
+    return (
+      <div>
+        <img src="https://drive.google.com/open?id=1MfFPj4TPic8X7RvKkZBjpTftFiz2048W"/>
+      </div>
+    );
+  }
+}
+
+export default login;
